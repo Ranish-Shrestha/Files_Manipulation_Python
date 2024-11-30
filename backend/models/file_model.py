9e781060caf1d7ea380c1dae1707c87f770490a8
Ranish Shrestha <35862852+Ranish-Shrestha@users.noreply.github.com>
2024-11-30 20:06:09 +0000

    added mongodb uri

--- backend/models/file_model.py
+++ backend/models/file_model.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 from bson import ObjectId
 
-client = MongoClient('mongodb://localhost:27017/')
+client = MongoClient('mongodb+srv://ranishstha:NjIXxKkVbbScg34q@filescluster.vi3ts.mongodb.net/?retryWrites=true&w=majority&appName=filesCluster')
 db = client['fileDB']
 
 file_collection = db['files']
